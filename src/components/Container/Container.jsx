2c09983a79341e5dbe86da20a32b71a0d22d21bd
Yash <yashkd12790@gmail.com>
2024-04-19 19:28:58 +0530

    commit

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,5 +1,3 @@
-
-
 function Container({ children }) {
   return <div className="w-full max-w-7xl mx-auto px-4">{children}</div>;
 }
--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -1,5 +1,5 @@
 function Logo({ width = "100px" }) {
-  return <div>Logo</div>;
+  return <div>BLOG</div>;
 }
 
 export default Logo;
