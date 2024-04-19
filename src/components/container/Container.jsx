2f032b969df237395a288d9298d78ca695601921
spiderkd <121319962+spiderkd@users.noreply.github.com>
2024-04-19 21:29:13 +0530

    Update Container.jsx
    type in container

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -1,6 +1,6 @@
 function Container({ children }) {
   return (
-    <div className="w-full max-w-7xl mx-auto px-4 bg-black ">{children}</div>
+    <div className="w-full max-w-7xl mx-auto px-4 ">{children}</div>
   );
 }
 
